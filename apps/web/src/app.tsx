12b7f6ad1d92f03bd3ea0b1413df6013b15287fa
Omar Diab <me@omardiab.com>
2024-11-02 20:39:16 +0900

    bun config and format

--- apps/web/src/app.tsx
+++ apps/web/src/app.tsx
@@ -6,18 +6,20 @@ import "./app.css";
 import { css } from "~gen/pandacss/css";
 
 export default function App() {
-  return (
-    <Router
-      root={(props) => (
-        <MetaProvider>
-          <Title>SolidStart - Basic</Title>
-          <a href="/" class={css({ color: "red", fontSize: "2xl" })}>Index</a>
-          <a href="/about">About</a>
-          <Suspense>{props.children}</Suspense>
-        </MetaProvider>
-      )}
-    >
-      <FileRoutes />
-    </Router>
-  );
+	return (
+		<Router
+			root={(props) => (
+				<MetaProvider>
+					<Title>SolidStart - Basic</Title>
+					<a href="/" class={css({ color: "red", fontSize: "2xl" })}>
+						Index
+					</a>
+					<a href="/about">About</a>
+					<Suspense>{props.children}</Suspense>
+				</MetaProvider>
+			)}
+		>
+			<FileRoutes />
+		</Router>
+	);
 }
