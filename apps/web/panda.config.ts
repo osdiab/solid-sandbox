bb1e31d01dab36e584b793bf0aed50a798046669
Omar Diab <me@omardiab.com>
2024-11-03 21:50:34 +0900

    set up button css

--- apps/web/panda.config.ts
+++ apps/web/panda.config.ts
@@ -1,5 +1,8 @@
 import { defineConfig } from "@pandacss/dev";
 
+const buttonAccentColorVariable = "--accent-color";
+const buttonTextOnAccentBackgroundColorVariable = "--text-on-accent-bg-color";
+
 export default defineConfig({
 	// Whether to use css reset
 	preflight: true,
@@ -12,7 +15,56 @@ export default defineConfig({
 
 	// Useful for theme customization
 	theme: {
-		extend: {},
+		extend: {
+			recipes: {
+				button: {
+					base: {
+						borderRadius: "sm",
+						paddingX: "2",
+						paddingY: "1",
+						border: "1px solid",
+						cursor: "pointer",
+					},
+					variants: {
+						role: {
+							primary: {
+								[buttonAccentColorVariable]: "token(colors.blue.500)",
+								[buttonTextOnAccentBackgroundColorVariable]: "white",
+							},
+							secondary: {
+								[buttonAccentColorVariable]: "token(colors.gray.500)",
+								[buttonTextOnAccentBackgroundColorVariable]: "white",
+							},
+							danger: {
+								[buttonAccentColorVariable]: "token(colors.red.500)",
+								[buttonTextOnAccentBackgroundColorVariable]: "white",
+							},
+						},
+						type: {
+							filled: {
+								background: `var(${buttonAccentColorVariable})`,
+								borderColor: `var(${buttonAccentColorVariable})`,
+								color: `var(${buttonTextOnAccentBackgroundColorVariable})`,
+							},
+							outline: {
+								background: "transparent",
+								borderColor: `var(${buttonAccentColorVariable})`,
+								color: `var(${buttonAccentColorVariable})`,
+							},
+							text: {
+								background: "transparent",
+								borderColor: "transparent",
+								color: `var(${buttonAccentColorVariable})`,
+							},
+						},
+					},
+					defaultVariants: {
+						role: "primary",
+						type: "filled",
+					},
+				},
+			},
+		},
 	},
 
 	// The output directory for your css system
