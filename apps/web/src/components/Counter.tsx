bdb6f7680ee99d257462366e6d40671104bd348b
Omar Diab <me@omardiab.com>
2024-11-03 21:51:04 +0900

    implement counter styles with panda

--- apps/web/src/components/Counter.tsx
+++ apps/web/src/components/Counter.tsx
@@ -1,15 +1,15 @@
-import { createSignal } from "solid-js";
-import "./Counter.css";
+import { type Component, createSignal } from "solid-js";
+import { button } from "~gen/pandacss/recipes";
 
-export default function Counter() {
+export const Counter: Component = () => {
 	const [count, setCount] = createSignal(0);
 	return (
 		<button
-			class="increment"
+			class={button()}
 			onClick={() => setCount(count() + 1)}
 			type="button"
 		>
 			Clicks: {count()}
 		</button>
 	);
-}
+};
